f960cf71b6706d5588a1285791c0295f81f2520a
Doğukan Ünal <99288560+dogukanma@users.noreply.github.com>
2023-09-25 21:01:44 +0300

    Update DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -16,7 +16,7 @@ public class DataTypes {
       try {
         long x = sc.nextLong();
         System.out.println(x + " can be fitted in:");
-        if (x >= -Byte.MIN_VALUE && x <= Byte.MAX_VALUE)
+        if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE)
           System.out.println("* byte");
         if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE)
           System.out.println("* short");
